1c7943c1b0444695677fe3fed438982f40d57d79
Janani Sundaragandhi <janani.s@multicorewareinc.com>
2021-11-29 16:19:03 +0530

    MetaWareNN-Backend: Move models download script & Envirnment setting script to mwnn_inference folder

--- mwnn_inference/env.sh
+++ mwnn_inference/env.sh
@@ -2,12 +2,12 @@
 
 ########### Executable Networks flow ##############
 #set the path to GLOW
-export FRAMEWORK_PATH=/Path/to/glow/
+export FRAMEWORK_PATH=/path/to/glow/
 export METAWARENN_LIB_PATH=$FRAMEWORK_PATH"/lib/Backends/MetaWareNN/metawarenn_lib/"
 export EXEC_DUMPS_PATH=$FRAMEWORK_PATH"/EXEC_DUMPS/"
 
 ########### NNAC - EV binary generation flow ##############
 #set the path to ARC directory
 export ARC_PATH=/path/to/ARC/
-export EV_CNNMODELS_HOME=/Path/to/cnn_models/
+export EV_CNNMODELS_HOME=/home/mcw/new_synopsys/arc_MWDT_EV/ARC/cnn_tools/cnn_models/
 export NNAC_DUMPS_PATH=$FRAMEWORK_PATH"/NNAC_DUMPS/"
